39a982b65e3f8bfa6088b9dccda3dbc1d07860b4
Pradeep <webreinvent@gmail.com>
2020-11-16 15:16:00 +0530

    Fixed: version from composer file

--- Config/config.php
+++ Config/config.php
@@ -1,27 +1,26 @@
 <?php
+$composer_data = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
 
-$config = array();
+$settings = [
+    "title"=> "Bootstrap 4.4 theme",
+    "slug"=> "btfourpointfour",
+    "thumbnail"=> "https://placehold.jp/300x160.png",
+    "excerpt"=> "Bootstrap 4.4 theme",
+    "description"=> "Bootstrap 4.4 theme",
+    "download_link"=> "",
+    "author_name"=> "btfourpointfour",
+    "author_website"=> "https://vaah.dev",
+    "version"=> $composer_data['version'],
+    "is_migratable"=> true,
+    "is_sample_data_available"=> true,
+    "db_table_prefix"=> "vh_btfourpointfour_",
+    "providers"=> [
+        "\\VaahCms\\Themes\\BtFourPointFour\\Providers\\BtFourPointFourServiceProvider"
+    ],
+    "aside-menu-order"=> null
+];
 
-/*
-|--------------------------------------------------------------------------
-| Get Theme Settings
-|--------------------------------------------------------------------------
-*/
-$path = __DIR__."/../settings.json";
-if (File::exists($path)) {
-    $file = File::get($path);
-    $plugin_config = json_decode($file);
-    $config = (array)$plugin_config;
-}
+$config_data = array_merge($composer_data, $settings);
 
-/*
-|--------------------------------------------------------------------------
-| Custom Theme Settings
-|--------------------------------------------------------------------------
-*/
 
-$theme_settings = [];
-
-$config = array_merge($config, $theme_settings);
-
-return $config;
+return $config_data;
