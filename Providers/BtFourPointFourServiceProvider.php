d22d027ae994e1b72e3f1abb57977957e839adc2
Pradeep <webreinvent@gmail.com>
2021-01-04 17:49:29 +0530

    Fixed: Disabled factories for laravel 8

--- Providers/BtFourPointFourServiceProvider.php
+++ Providers/BtFourPointFourServiceProvider.php
@@ -28,7 +28,7 @@ class BtFourPointFourServiceProvider extends ServiceProvider
         $this->registerConfig();
         $this->registerViews();
         $this->registerAssets();
-        $this->registerFactories();
+        //$this->registerFactories();
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
         $this->registerSeeders();
         $this->registerBladeDirectives();
